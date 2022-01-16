0a3af99e49becd6b805fe0a51b85e59ea4c0585d
cpele <geekarist@users.noreply.github.com>
2022-01-16 14:28:36 +0100

    Split od/update function

--- src/app/renderer/orig_dest.cljs
+++ src/app/renderer/orig_dest.cljs
@@ -22,28 +22,34 @@
        (dispatch-to-od [::msg.change-dest (-> event .-target .-value)]))}]
    [:button.od-search {:on-click #(dispatch-to-od [::msg.submit])} "Search" ]])
 
+(defn- update-on-submit [state]
+  (let [effect-key ::fx/http-request
+        effect-arg {:uri (str "http://localhost:3000/journeys"
+                              "?request.from=2.3749036;48.8467927"
+                              "&request.to=2.2922926;48.8583736")
+                    :method :get
+                    :format (json-request-format)
+                    :response-format (json-response-format)}
+        effect-target ::msg.http-resp-received] 
+    [state
+     [effect-key [effect-arg effect-target]]]))
+
+(defn- update-on-change-dest [state msg-arg]
+  [(assoc state ::state.dest msg-arg)
+     nil])
+
+(defn- update-on-change-orig [state msg-arg]
+  [(assoc state ::state.orig msg-arg)
+     nil])
+
+(defn- update-default [state msg]
+  [state [::fx/log (str "Unknown message in orig_dest: " msg)]])
+
 (defn update [state [msg-key msg-arg :as msg]]
   (condp = msg-key
-    
-    ::msg.change-orig
-    [(assoc state ::state.orig msg-arg)
-     nil]
-    
-    ::msg.change-dest
-    [(assoc state ::state.dest msg-arg)
-     nil]
-    
-    ::msg.submit
-    [state
-     [::fx/http-request
-      [{:uri (str "http://localhost:3000/journeys"
-                  "?request.from=2.3749036;48.8467927"
-                  "&request.to=2.2922926;48.8583736")
-        :method :get
-        :format (json-request-format)
-        :response-format (json-response-format)}
-      ::msg.http-resp-received]]]
-    
-    [state [::fx/log (str "Unknown message in orig_dest: " msg)]]))
+    ::msg.change-orig (update-on-change-orig state msg-arg)
+    ::msg.change-dest (update-on-change-dest state msg-arg)
+    ::msg.submit (update-on-submit state)
+    (update-default state msg)))
 
 (comment init view update)
